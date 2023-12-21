5e8f4b9a7b33c616bd339cb39b847d9fd85e4770
steve-anunknown <steve.anunknown@gmail.com>
2023-12-21 17:15:51 +0200

    rewrote the byte functions

--- utils.c
+++ utils.c
@@ -10,10 +10,10 @@ uintptr_t get1Byte(void *ptr)
 {
     uint8_t *address = ptr;
     uintptr_t result = 0;
-    result = (*address) & (0x00000000000000FF);
-    if ((*address) & 0x0000000000000080)
-        // sign extend it.
-        result = result | (0x7FFFFFFFFFFFFF00); 
+    result = *address;
+    result = result & 0xFF;
+    if (result & 0x80)
+        result = result | 0xFFFFFFFFFFFFFF00;
     return result;
 }
 uintptr_t get2Byte(void *ptr)
@@ -22,7 +22,7 @@ uintptr_t get2Byte(void *ptr)
     uint8_t first_byte = *address;
     uint8_t secnd_byte = *(address + 1);
     uintptr_t result = 0;
-    result = ((secnd_byte << SHIFT_1_BYTE) + first_byte);
+    result = ((secnd_byte << SHIFT_1_BYTE) | first_byte);
     if (secnd_byte & 0x00000000000080)
         // sign extend it.
         result = result | (0x7FFFFFFFFFFF0000);
@@ -31,27 +31,21 @@ uintptr_t get2Byte(void *ptr)
 uintptr_t get2ByteAddress(void *ptr)
 {
     uint8_t *address = ptr;
-    uint8_t first_byte = *address;
-    uint8_t secnd_byte = *(address + 1);
-    uintptr_t result = 0;
-    result = ((secnd_byte << SHIFT_1_BYTE) + first_byte);
+    uintptr_t result = (address[1] << SHIFT_1_BYTE) | address[0];
     return result;
 }
 uintptr_t get4Byte(void *ptr)
 {
     uint8_t *address = ptr;
-    uint8_t first_byte = *address;
-    uint8_t secnd_byte = *(address+1);
-    uint8_t third_byte = *(address+2);
-    uint8_t forth_byte = *(address+3);
     uintptr_t result = 0;
-    result = ((forth_byte << SHIFT_3_BYTE) +
-            (third_byte << SHIFT_2_BYTE) + 
-            (secnd_byte << SHIFT_1_BYTE) +
-            first_byte);
-    if (forth_byte & 0x00000000000080)
-        // sign extend it.
-        result = result | (0x7FFFFFFF00000000);
-    result = result & GC_MASK;
+    result |= address[0];
+    result |= (uintptr_t)address[1] << SHIFT_1_BYTE;
+    result |= (uintptr_t)address[2] << SHIFT_2_BYTE;
+    result |= (uintptr_t)address[3] << SHIFT_3_BYTE;
+    
+    if (address[3] & 0x80)
+        result |= (uintptr_t)0xFFFFFFFF00000000;
+    
+    result &= GC_MASK;
     return result;
 }
